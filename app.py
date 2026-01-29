9a325e86a7a8d7a388f23363c03ba9ee78a0f11b
Rahulrai23 <76586652+Rahulrai23@users.noreply.github.com>
2026-01-29 16:27:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,7 @@
 def scheme_rajasthan():
     return render_template("scheme_rajasthan.html")
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
