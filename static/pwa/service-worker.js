53f9db0bcc5c229a1c64833fb3a3ff9a00165a9d
Rahulrai23 <76586652+Rahulrai23@users.noreply.github.com>
2026-01-30 12:01:22 +0530

    Update service-worker.js

--- static/pwa/service-worker.js
+++ static/pwa/service-worker.js
@@ -1,1 +1,26 @@
+const CACHE_NAME = "kc-scheme-v1";
+
+const OFFLINE_ASSETS = [
+  "/scheme",
+  "/static/pwa/manifest.json",
+  "/static/pdfs/haryana.pdf",
+  "/static/pdfs/karnataka.pdf",
+  "/static/pdfs/rajasthan.pdf"
+];
+
+// INSTALL
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_ASSETS))
+  );
+});
+
+// FETCH
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
